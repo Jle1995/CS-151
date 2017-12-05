1e466d4296bfd5943c0292e2f4045dc06b1e7242
Jle1995 <31541518+Jle1995@users.noreply.github.com>
2017-12-04 19:54:04 -0500

    Update CS151-Assignment5.cpp

--- CS151-Assignment5.cpp
+++ CS151-Assignment5.cpp
@@ -6,39 +6,39 @@ using namespace std;
 //Return: void
 //Description: Function for guess a random number.
 //flowchart:
-//Label												Task												Goto					
-//01										(start: int &min, int &max;) 								 02
-//02											  [int x;]												 03
-//03											[int random;]											 04
-//04									   /"Guess my number: ";/										 05
-//05									         /random;/												 06
-//06										<while (x < random)>										 07, 14
-//07										 [srand(time(0));]											 08
-//08									  [x = ((rand ()% max));]										 09
-//09									  <while (!(x == random))>										 10, 12
-//10										/"Incorrect. Guess Again ";/								 11
-//11									         /"random";/											 12
-//12								          <if (x == random)>										 13
-//13										   /"You got it!";/											 14
+//Label												Task												                  Goto					
+//01										(start: int &min, int &max;) 				                 02
+//02											  [int x;]												                 03
+//03											[int random;]											                 04
+//04									   /"Guess my number: ";/							                 05
+//05									         /random;/										                 06
+//06										<while (x < random)>								                 07, 14
+//07										 [srand(time(0));]									                 08
+//08									  [x = ((rand ()% max));]							                 09
+//09									  <while (!(x == random))>						                 10, 12
+//10										/"Incorrect. Guess Again ";/				                 11
+//11									         /"random";/									                 12
+//12								          <if (x == random)>						                 13
+//13										   /"You got it!";/									                 14
 //14											(return void;)
 
-Name: main()
+//Name: main()
 //Parameters: None
 //Return: None
 //Description: Main function to enter two numbers and have the user guess a number from the program.
 //flowchart:
-//Label												Task												Goto
+//Label												Task												                    Goto
 //01							 (start: int x, int random, int max, int min;)							 02
-//02										/"Enter 2 numbers: ";/										 03
-//03												/x;/												 04
-//04											  /random;/												 05
-//05									 [GuessNumber(x, random);]										 06
-//06										 <if (x > random)>											 07, 09
-//07											 [min = x;]												 08
-//08										   [max = random;]											 09
-//09									    <else if (random > x)>										 10, 12
-//10											[max = random;]											 11
-//11											 [min = x;]												 12
+//02										/"Enter 2 numbers: ";/										             03
+//03												/x;/												                       04
+//04											  /random;/												                   05
+//05									 [GuessNumber(x, random);]										           06
+//06										 <if (x > random)>											               07, 09
+//07											 [min = x;]												                   08
+//08										   [max = random;]											               09
+//09									    <else if (random > x)>										           10, 12
+//10											[max = random;]											                 11
+//11											 [min = x;]												                   12
 //12											 (return 0;)
 
 void GuessNumber(int &min, int &max)
